bc0f2caa3bec50b2c4a7a1695746f8da7a297b9f
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-07-30 17:52:23 +0900

    feat(notifications): provide shared notification state

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
+import { NotificationProvider } from './context/NotificationContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <NotificationProvider>
+          <App />
+        </NotificationProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
