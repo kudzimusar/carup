c7b911ddf84242b7614c20949910f194da8c2b99
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-07-30 18:05:35 +0900

    refactor(notifications): mount isolated provider component

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
-import { NotificationProvider } from './context/NotificationContext.tsx'
+import { NotificationProvider } from './context/NotificationProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
